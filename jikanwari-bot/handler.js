d271bb1289c7eb62582e1ef447457eb8fd074567
shinbunbun <takamuneyuto@gmail.com>
2020-06-07 14:53:54 +0900

    some fix()

--- jikanwari-bot/handler.js
+++ jikanwari-bot/handler.js
@@ -7,7 +7,7 @@ const client = new line.Client({
 const AWS = require('aws-sdk');
 const axios = require('axios');
 
-module.exports.hello = async (event, context) => {
+module.exports.hello = (event, context) => {
 
   let checkHeader = (event.headers || {})['X-Line-Signature'];
 
